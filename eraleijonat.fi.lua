6510c64b527b3ae5a394dff3bdce39fc430ce2ac
Joonas Rouhiainen <joonas.rouhiainen@reaktor.com>
2020-01-04 15:44:26 +0200

    Migrate Erä-Leijonat domain to eraleijonat.fi

---
+++ eraleijonat.fi.lua
@@ -0,0 +1,5 @@
+a("eraleijonat.fi", "185.199.108.153")
+a("eraleijonat.fi", "185.199.109.153")
+a("eraleijonat.fi", "185.199.110.153")
+a("eraleijonat.fi", "185.199.111.153")
+cname("www.eraleijonat.fi", "eraleijonat.github.io")
