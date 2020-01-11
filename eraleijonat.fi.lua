cc9abb44f879c4995825e2cc930122d6f6c8055c
Joonas Rouhiainen <joonas.rouhiainen@reaktor.com>
2020-01-11 13:58:12 +0200

    google-site-verification

--- eraleijonat.fi.lua
+++ eraleijonat.fi.lua
@@ -3,3 +3,5 @@ a("eraleijonat.fi", "185.199.109.153")
 a("eraleijonat.fi", "185.199.110.153")
 a("eraleijonat.fi", "185.199.111.153")
 cname("www.eraleijonat.fi", "eraleijonat.github.io")
+txt("eraleijonat.fi", "google-site-verification=Sk-4oScyToTT3kVfiTGVbQXha_4vfLkAy_L5H-CuoCY")
+
